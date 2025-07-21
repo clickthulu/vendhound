d70fbdf9d24d6bd2fb839cc7ef692fe0574e33b0
Jesse <neubauje@gmail.com>
2025-07-20 20:59:50 -0400

    Added an enum for user roles

--- app/src/Enumerations/ImageUsageType.php
+++ app/src/Enumerations/ImageUsageType.php
@@ -11,4 +11,16 @@ class ImageUsageType implements OptionEnumerationInterface
     const IMAGE_AD = 'Advertisement';
     const IMAGE_OTHER = 'Some other purpose';
 
+public function toArray(): array
+    {
+        return [
+            self::IMAGE_LOGO => self::IMAGE_LOGO,
+            self::IMAGE_BANNER => self::IMAGE_BANNER,
+            self::IMAGE_SETUP => self::IMAGE_SETUP,
+            self::IMAGE_PORTFOLIO => self::IMAGE_PORTFOLIO,
+            self::IMAGE_AD => self::IMAGE_AD,
+            self::IMAGE_OTHER => self::IMAGE_OTHER
+        ];
+    }
+
 }
